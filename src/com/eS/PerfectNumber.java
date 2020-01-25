48da86b5ae31ebb189e197d52217258115ad1008
es <StaNC2000>
2020-01-25 11:52:41 +0100

    Method isPerfectNumber created and tested

--- src/com/eS/PerfectNumber.java
+++ src/com/eS/PerfectNumber.java
@@ -2,7 +2,35 @@ package com.eS;
 
 public class PerfectNumber {
 
+    public static boolean isPerfectNumber(int number){
+        int divider = 1;
+        int dividerSummary = 0;
+
+        if (number < 1){
+            return false;
+        }
+
+        for (divider = 1; divider < number; divider++){
+            if (number % divider == 0){
+                dividerSummary = dividerSummary + divider;
+//                System.out.println("Divider "  + divider + " dividerSummary " + dividerSummary + " number " + number);
+                if (dividerSummary == number){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+
     public static void main(String[] args) {
 	// write your code here
+
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(-6));
+        System.out.println(isPerfectNumber(31));
+        System.out.println(isPerfectNumber(6666));
     }
 }
